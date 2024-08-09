3abed3b635b97862d376801c129eb587fe1eade0
stanleyoos <stanislaw.sarka@gmail.com>
2024-08-09 14:42:34 +0200

    feat: Created screen - pass props

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,64 @@
-import { ScrollView, View, Platform } from "react-native";
+// In App.js in a new project
 
-import { Article } from "../Shared/Article";
+import * as React from "react";
+import { View, Text, Button } from "react-native";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export type NativeStackParams = {
-  Article: { author: string } | undefined;
-};
-
-const ArticleScreen = () => {
+function HomeScreen({ navigation }: { navigation: any }) {
   return (
-    <View>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Article author={{ name: "Gandalf" }} />
-      </ScrollView>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() =>
+          navigation.navigate("Details", {
+            itemId: 85,
+            otherParam: "anything you want here",
+          })
+        }
+      />
     </View>
   );
-};
+}
 
-export default function Index() {
-  const Stack = createNativeStackNavigator<NativeStackParams>();
+function DetailsScreen({ navigation, route }: { navigation: any; route: any }) {
+  const { itemId, otherParam } = route.params;
   return (
-    <Stack.Navigator initialRouteName="Article">
-      <Stack.Screen
-        name="Article"
-        component={ArticleScreen}
-        options={{
-          headerTransparent: true,
-          headerBlurEffect: "light",
-          headerLargeTitle: true,
-        }}
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 22, marginBottom: 20 }}>Details Screen</Text>
+      <Text style={{ fontSize: 30 }}>itemId: {JSON.stringify(itemId)}</Text>
+      <Text style={{ fontSize: 22, marginBottom: 100 }}>
+        otherParam: {JSON.stringify(otherParam)}
+      </Text>
+
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push("Details", {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
       />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      {/* <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      /> */}
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
     </Stack.Navigator>
   );
 }
+
+export default App;
